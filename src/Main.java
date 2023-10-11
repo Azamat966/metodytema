51784f7d609bbd03f6de6afd0681cabb439bbd84
Azamat966 <reallygoooool01@gmail.com>
2023-10-11 17:59:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    }
+//        calkulate();
+//    }
+//    public static void calkulate(){
+//        for (int i = 1; i < 10; i++) {
+//            for (int j = 1; j <= 10; j++) {
+//                System.out.println(" ");
+//                System.out.println(i + "*" + i + "=" + i * i);
+
+  //          }
+
+  //      }
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        if (a == 1) {
+//            hello();
+//        if(a == 0) {
+//            bye();
+//        }else
+//        System.out.println("tuura emes")
+//
+//        }
+//    }
+//    public static void hello(){
+//        System.out.println("Hello");
+//    }
+//    public static void bye(){
+//        System.out.println("Корушкончо");
+
+
+//
+////}       calculate();
+//    }
+//    public static void calculate(){
+//        System.out.println("АСАЛЯМУ АЛЕЙКУМ"); //1задания
+
+
